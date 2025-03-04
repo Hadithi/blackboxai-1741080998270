05b3ba43d89b39dda84cba5d0b8d6f9e46fc7b88
Hadithi <abdulazim.ray@gmail.com>
2025-03-04 13:07:27 +0330

    Add ecommerce/frontend/src/redux/store.js 1741081046885

---
+++ ecommerce/frontend/src/redux/store.js
@@ -0,0 +1,37 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import authReducer from './slices/authSlice';
+import cartReducer from './slices/cartSlice';
+import productReducer from './slices/productSlice';
+import orderReducer from './slices/orderSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth', 'cart'], // Only persist these slices
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  cart: cartReducer,
+  product: productReducer,
+  order: orderReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
